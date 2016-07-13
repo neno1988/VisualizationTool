f42f764a24858057035f86e6ca7f65475623b775
neno1988 <lorenzogarbani@hotmail.com>
2016-07-13 13:38:38 +0200

    Added readme

---
+++ ReadMe.m
@@ -0,0 +1,18 @@
+% This tool offers a nice way to represent data in the field of rain/wastewwater
+% 
+% data structure in catchmentInfo
+% 
+% for each RUEB: x, y position
+%                 fill level data
+%                 flow data
+%                 quality data
+%                 ...
+% name of below structure
+% position from ARA: 1.2.1.1.1
+% .mat files around: time (seconds) and data as TS with standard names:
+% (RUEB_NAME).timeSec
+%            .waterLevel
+%            .flow
+%            .COD
+%            ...
+%                  
